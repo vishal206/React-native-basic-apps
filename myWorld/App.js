ecc12ce6ff1a01adac8d89d10ea827de3d00ec57
vishal R <vishal206ani@gmail.com>
2021-11-03 14:22:05 +0530

    completed hello world

--- myWorld/App.js
+++ myWorld/App.js
@@ -6,7 +6,7 @@
  * @flow strict-local
  */
 
-import React from 'react';
+import React,{useState} from 'react';
 import type {Node} from 'react';
 import {
   SafeAreaView,
@@ -14,6 +14,7 @@ import {
   StatusBar,
   StyleSheet,
   Text,
+  TextInput,
   useColorScheme,
   View,
 } from 'react-native';
@@ -26,87 +27,45 @@ import {
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
+const App=()=>{
+  const[text,setText]=useState('WORLD');
+  
+  return(
+    <View style={styles.container}>
+      <Text style={styles.mytext}>HELLO {text}</Text>
+      <View>
+      <TextInput style={styles.myTextInput}
+      placeholderTextColor='grey'
+      autoCapitalize="characters"
+      onChangeText={text => setText(text)}
+      />
+      </View>
     </View>
   );
-};
-
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-};
+}
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  container:{
+    flex:1,
+    backgroundColor:'white',
+    justifyContent:'center',
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  mytext:{
+    color: 'black',
+    fontSize: 30,
+    fontWeight:'200',
+    textAlign:'center',
+    marginBottom: 20,
+    letterSpacing: 4
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
+  myTextInput:{
+    color:'black',
+    fontSize: 30,
+    borderWidth:1,
+    textAlign:'center',
+    marginHorizontal:10,
+    fontWeight:'900',
+  }
 });
 
 export default App;
