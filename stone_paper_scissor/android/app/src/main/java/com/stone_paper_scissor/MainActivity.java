6e744a187d5f4bb98fc97e6e47348b8e69d2fa37
vishal R <vishal206ani@gmail.com>
2021-11-05 21:23:32 +0530

    adding stone_paper_scissor

---
+++ stone_paper_scissor/android/app/src/main/java/com/stone_paper_scissor/MainActivity.java
@@ -0,0 +1,15 @@
+package com.stone_paper_scissor;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "stone_paper_scissor";
+  }
+}
